1dfcbee9560f460f51d7253833c17cc3143af8e4
kiorky <devnull@localhost>
2008-05-19 18:08:12 +0200

    0.4 changes

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -1,5 +1,17 @@
 import os
 
-def getpostgresqlenv(options,buildout):
-    os.environ['EXTRA_LDAP_LIBS'] = os.environ['LDFLAGS']
+uname=os.uname()[0]
+
+def getopenldapenv(options,buildout):
+    #on everything else than linux patch configure too
+    if uname in ['Darwin', 'FreeBSD']:
+        cmd1 = "gsed \
+                -e 's|-lsasl2||' \
+                -e 's|-lsasl||' \
+                -i %s/*/configure" % (options['compile-directory'])
+
+        if os.system(cmd1):
+                raise Error('System error')
+
+
 # vim:set ts=4 sts=4 et  :
